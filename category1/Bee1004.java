4add3eb58ad1e4f83b3fff94495fd9a0a1980d8d
RuanPablo <ruan.santos@ab-inbev.com>
2024-03-01 17:18:55 -0300

    Bee 1004 - Simple Product

---
+++ category1/Bee1004.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bee1004 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int b = scanner.nextInt();
+
+        int prod = a * b;
+
+        System.out.println("PROD = " + prod);
+    }
+}
