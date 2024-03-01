1200e8688563464f89e86479c24e2f1bc4cdc7ac
RuanPablo <ruan.santos@ab-inbev.com>
2024-03-01 17:15:34 -0300

    Bee 1003 - Simple Sum

---
+++ category1/Bee1003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Bee1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int b = scanner.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("SOMA = " + soma);
+    }
+}
